8ae45bbecb8d26775c5e981e1cf19c8978d456b8
Daniil <gol.daniel30@gmail.com>
2024-12-03 09:09:08 +0100

    fgfdg

---
+++ C#/first_code_cs.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program{
+    static void Main(string[] args){
+        // pobieranie imienia
+        Console.Write("Podaj swoje imię: ");
+        string imie = Console.ReadLine();
+
+        Console.Write("Podaj swoję nazwisko: ");
+        string nazwisko = Console.ReadLine();
+
+        Console.Write("Podaj ile masz lat");
+        string wiek = Console.ReadLine();
+
+        
+    }
+}
